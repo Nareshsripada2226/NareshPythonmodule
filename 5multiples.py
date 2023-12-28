6a66f4f06b7301b957928df0035aa728a0899a5b
Nareshsripada2226 <154877980+Nareshsripada2226@users.noreply.github.com>
2023-12-28 14:13:03 +0530

    Add files via upload

---
+++ 5multiples.py
@@ -0,0 +1,5 @@
+num = int(input())
+if num%5==0:
+    print(f'Given number {num} is multiple of 5')
+else :
+    print(f'Given number {num} is not multiple of 5')
---
+++ addtwo.py
@@ -0,0 +1,3 @@
+a= 35
+b= 45
+print("Addition of two numbers",a+b)
